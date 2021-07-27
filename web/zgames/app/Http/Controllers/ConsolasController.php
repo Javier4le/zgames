5c93cd7f740fb808c3de7968b94c91c42f3a4b74
Javier <javier4le.dev@gmail.com>
2021-07-27 02:36:08 -0400

    Generar modelo y tabla para consola

--- web/zgames/app/Http/Controllers/ConsolasController.php
+++ web/zgames/app/Http/Controllers/ConsolasController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Consola;
 //Aqui va a ir toda la logica vinculada a las consolas
 class ConsolasController extends Controller
 {
@@ -15,9 +15,33 @@ class ConsolasController extends Controller
 
         return $marcas;
     }
+    /**
+     * Esta funcion va a ir a buscar todas las consolas que existen en la BD
+     * y las va a retornar
+     */
+    public function getConsolas(){
+        //Equivalente a un select * from consolas\
+        $consolas = Consola::all();
+        return $consolas;
+    }
+    /**
+     * Esta funcion va a registrar una consola de ejemplo en la BD
+     * TODO: Mejorar esto para que no sea un ejemplo
+     */
+    public function crearConsola(){
+        //Equivalente a un insert into bla bla
+        $consola = new Consola();
+        $consola->nombre = "Nintendo Switch";
+        $consola->marca = "Nintendo";
+        $consola->anio = 2015;
+
+        $consola->save();
+        return $consola;
+    }
+
 }
 // view productos.blade.php
-// renderizar los productos 
+// renderizar los productos
 
 //ProductosController:
   //  - ir a buscar los productos a la bd
